9ef4971b5551d19d64be471f9a9c8a8ae32c3ab9
abhinavkaushik-rgb <abhinav.kaushik@antino.com>
2026-01-17 03:08:05 +0530

    cors addon

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import connectToMongo from "./database/db.js";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
 connectToMongo();
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Example app listening the http://localhost:${port}`);
