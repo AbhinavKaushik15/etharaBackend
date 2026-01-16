f1c2203daff1b583655996780f2edf84d2463618
abhinavkaushik-rgb <abhinav.kaushik@antino.com>
2026-01-17 02:24:11 +0530

    remove the attendance route

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import express from "express";
 import connectToMongo from "./database/db.js";
-import AttendanceManagement from './../ethara.ai/src/pages/AttendanceManagement';
 
 const app = express();
 const port = 5000;
 connectToMongo();
-
-app.use("/api/attendance", attendance)
 
 app.listen(port, () => {
   console.log(`Example app listening the http://localhost:${port}`);
